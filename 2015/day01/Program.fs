e1c8ea382b3e4362d0228f23488f56e0c71ce88d
Mikael Frosthage <mikael.frosthage@zimpler.com>
2023-12-25 22:29:35 +0100

    day01

---
+++ 2015/day01/Program.fs
@@ -0,0 +1,17 @@
+﻿
+open System.IO
+
+File.ReadAllText("input").ToCharArray()
+|> Array.fold (fun acc c -> if c = '(' then acc + 1 else acc - 1) 0
+|> printfn "Part 1: %d"
+
+
+File.ReadAllText("input").ToCharArray()
+|> Array.scan (fun acc c -> if c = '(' then acc + 1 else acc - 1) 0
+|> Array.findIndex (fun x -> x = -1)
+|> printfn "Part 2: %d"
+
+
+
+
+
