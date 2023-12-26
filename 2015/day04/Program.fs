59f55cf72d4a1820075c2b2b790d02013e10891a
Mikael Frosthage <mikael.frosthage@zimpler.com>
2023-12-26 13:56:45 +0100

    day04

--- 2015/day04/Program.fs
+++ 2015/day04/Program.fs
@@ -1,4 +1,28 @@
 ﻿
-open System.IO
+open System
+open System.Security.Cryptography
+open System.Text
 
-let input = File.ReadAllLines("example")
+let md5 = MD5.Create()
+
+let hash (s:string) =
+    let bytes = Encoding.ASCII.GetBytes(s)
+    let hashedBytes = md5.ComputeHash(bytes)
+    BitConverter.ToString(hashedBytes).Replace("-", "").ToLower()
+      
+let input = "ckczppom"
+
+(Seq.initInfinite id
+|> Seq.map (fun x -> (x, hash (input + (string x))))
+|> Seq.takeWhile (fun (_, x) -> not (x.StartsWith("00000")))
+|> Seq.last
+|> fst) + 1
+|> printfn "Part 1: %A"
+
+(Seq.initInfinite id
+|> Seq.map (fun x -> (x, hash (input + (string x))))
+|> Seq.takeWhile (fun (_, x) -> not (x.StartsWith("000000")))
+|> Seq.last
+|> fst) + 1
+|> printfn "Part 2: %A"
+
