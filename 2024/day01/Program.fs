c6bbac885132437db8caac1f07f58850c4577077
Mikael Frosthage <mikael.frosthage@zimpler.com>
2024-12-01 16:15:41 +0100

    refactor: day01

--- 2024/day01/Program.fs
+++ 2024/day01/Program.fs
@@ -10,12 +10,10 @@ let p = parse input
 let sorted_c1 = fst p |> Array.sort
 let sorted_c2 = snd p |> Array.sort
 
-let part1 =
-    (Array.zip sorted_c1 sorted_c2)
-    |> Array.map (fun (a, b) -> abs (int a - int b))
-    |> Array.sum
-
-printf $"Part1: {part1}\n"
+(Array.zip sorted_c1 sorted_c2)
+|> Array.map (fun (a, b) -> abs (int a - int b))
+|> Array.sum
+|> printf "Part1: %A\n"
 
 let c2_map =
     sorted_c2
@@ -23,9 +21,7 @@ let c2_map =
     |> Array.map (fun (k, v) -> (k, v |> Array.length))
     |> Map.ofArray
 
-let part2 =
-    sorted_c1
-    |> Array.map (fun x -> Map.tryFind x c2_map |> Option.defaultValue 0 |> (*) x)
-    |> Array.sum
-
-printf $"Part2: %A{part2}"
+sorted_c1
+|> Array.map (fun x -> Map.tryFind x c2_map |> Option.defaultValue 0 |> (*) x)
+|> Array.sum
+|> printf "Part2: %A\n"
