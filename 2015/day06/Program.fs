b96406524e76aee10abf3b7a103531c5a86d1ad1
Mikael Frosthage <mikael.frosthage@zimpler.com>
2024-12-02 18:01:47 +0100

    day 02 2024

---
+++ 2015/day06/Program.fs
@@ -0,0 +1,11 @@
+﻿
+open System.IO
+
+let input = File.ReadAllLines("example")
+
+let lights = Array2D.zeroCreate<int> 1000 1000
+
+input |> 
+
+
+
---
+++ 2024/day02/Program.fs
@@ -0,0 +1,45 @@
+﻿open System.IO
+
+File.ReadAllLines("input")
+|> Array.map (fun x -> x.Split(' ') |> Array.map int)
+|> Array.filter (fun x ->
+    let d = x |> Array.windowed 2 |> Array.map (fun x -> x[1] - x[0])
+    Array.forall (fun x -> x > 0) d || Array.forall (fun x -> x < 0) d)
+|> Array.filter (fun x ->
+    let d = x |> Array.windowed 2 |> Array.map (fun x -> x[1] - x[0])
+    Array.forall (fun x -> abs (x) < 4) d)
+|> Array.length
+|> printf "Part1 %A\n"
+
+
+File.ReadAllLines("input2")
+|> Array.map (fun x -> x.Split(' ') |> Array.map int)
+|> Array.filter (fun l ->
+    [0..1..l.Length-1]
+    |> List.exists (fun i ->
+        let d =
+            l
+            |> Array.indexed
+            |> Array.filter (fun (i2, _) -> i <> i2)
+            |> Array.map snd
+            |> Array.windowed 2
+            |> Array.map (fun x -> x[1] - x[0])
+
+        let rule_1 = Array.forall (fun x -> x > 0) d || Array.forall (fun x -> x < 0) d
+        
+        let d2 =
+            l
+            |> Array.indexed
+            |> Array.filter (fun (i2, _) -> i <> i2)
+            |> Array.map snd
+            |> Array.windowed 2
+            |> Array.map (fun x -> x[1] - x[0])
+
+        let rule_2 = Array.forall (fun x -> abs x < 4) d2 
+        
+        rule_1 && rule_2
+        )
+    )
+|> Array.length
+|> printf "Part2 %A\n"
+
